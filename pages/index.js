f539eec3b6fba311f3b6fdebcaab5fd4f078097f
Regis Moura Dantas <dantas.regis.m@gmail.com>
2025-11-22 20:01:18 +0000

    Added prettier and fixed

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 function Home() {
-    return (<>
-        <h1>Olá Thaly! Eu te amo muito. ❤️</h1>
-        <h2>Se você me ama, da uma risadinha.</h2>
-    </>)
-
+  return (
+    <>
+      <h1>Olá Thaly! Eu te amo muito. ❤️</h1>
+      <h2>Se você me ama, da uma risadinha.</h2>
+    </>
+  );
 }
 
 export default Home;
